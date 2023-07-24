5537f868170f60cb3d24328afd6292462996460a
chaehoon <chaehoon_lim@kolon.com>
2023-07-24 10:13:03 +0900

    ...nextauth 추가

---
+++ next-env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+// https://github.com/nextauthjs/next-auth/discussions/536#discussioncomment-1932922
+import type { DefaultUser } from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user?: DefaultUser & {
+      id: string;
+    };
+  }
+}
