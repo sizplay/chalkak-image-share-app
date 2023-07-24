53294347820d5502c7d3b5c8ab8641c87163c7e7
chaehoon <chaehoon_lim@kolon.com>
2023-07-24 10:53:05 +0900

    next-auth.d.ts 추가

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,13 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-// https://github.com/nextauthjs/next-auth/discussions/536#discussioncomment-1932922
-import type { DefaultUser } from 'next-auth';
-
-declare module 'next-auth' {
-  interface Session {
-    user?: DefaultUser & {
-      id: string;
-    };
-  }
-}
